599c7a7ae28d5dce48062d88066245ca963a6c16
Melvin Schultz <melvinschultzdev@gmail.com>
2017-05-18 22:39:29 +0200

    fix date + display board

--- assets/js/myJs.js
+++ assets/js/myJs.js
@@ -1,19 +1,56 @@
-/*jslint browser:true*/
-'use strict';
+function getTimes() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.tfl.gov.uk/StopPoint/940GZZLUNFD/Arrivals?app_key=%20a73f7a36fab42a930bf7cf8b1616d995&app_id=be29441e',
+    dataType: 'Json',
+    success: function (data) {
+      $("#board").html("");
+      var sorted = data.sort(function (a, b) {
+        if (a.timeToStation > b.timeToStation) {
+          return 1;
+        }
+        if (a.timeToStation < b.timeToStation) {
+          return -1;
+        }
 
-var arrivalTime = new XMLHttpRequest();
-var tObj;
+        return 0;
+      });
 
-// GET THE ARRIVAL TIME
-arrivalTime.open('GET','https://api.tfl.gov.uk/StopPoint/940GZZLUNFD/Arrivals?app_key=%20a73f7a36fab42a930bf7cf8b1616d995&app_id=be29441e', true);
-arrivalTime.responseType = 'text';
-arrivalTime.send(null);
+      $.each(data, function (key, value) {
+        var time;
+        if (value.timeToStation < 900) {
+          if (value.timeToStation > 60) {
+            time = parseInt(value.timeToStation / 60) + "min";
+            $("#board").append(key + " /// " + value.lineName + " / " + value.stationName + " => " + value.destinationName + "<br>" + value.platformName + " " + time + "<br>");
+          } else {
+            time = "due";
+            $("#board").append(key + " /// " + value.lineName + " / " + value.stationName + " => " + value.destinationName + "<br>" + value.platformName + " " + time + "<br>");
+          }
+        }
+        console.log(key + " /// " + value.lineName + " / " + value.stationName + " => " + value.destinationName + " / " + value.platformName + " " + parseInt(value.timeToStation / 60) + "min");
+      });
+    }
+  });
+}
 
-arrivalTime.onload = function() {
-	if (arrivalTime.status === 200){
-		tObj = JSON.parse(arrivalTime.responseText);
-		console.log(tObj);
-		document.getElementById('board').innerHTML = tObj.timeToStation;
-	} //end if
-}; //end function
+function getClock() {
+  var date = new Date();
 
+  var options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  };
+
+  var dateFormatted = date.toLocaleTimeString("en-UK", options);
+
+  document.getElementById('clockbox').innerHTML = dateFormatted;
+}
+
+window.onload = function () {
+  getClock();
+  setInterval(getClock, 60000);
+}
